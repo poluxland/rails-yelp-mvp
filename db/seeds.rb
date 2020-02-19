7ca5df06c906054bde803e437d523cbab25ccc4d
Jose Jerez <joseignaciojerez@gmail.com>
2020-02-19 14:34:42 -0300

    rake ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+new_restaurant = Restaurant.new(
+name: Faker::Book.title,
+address: Faker::Address.street_address,
+phone_number: "123",
+category: "italian")
+
+new_restaurant.save
+end
